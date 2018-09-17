d60a5d16a0b536624945d1b90defc0869ce89fd4
yanivroz <yanivr@radware.com>
2018-09-17 08:13:18 +0300

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export BGP_LOCAL=$(ip -4 addr show `route -n | grep '^0.0.0.0' | awk '{print $NF}'` | grep -Eo 'inet [^/]+' |awk '{print $2}' | head -1)
-export BGP_PEER=$(route -n | grep '^0.0.0.0' | awk '{print $2}')
+export BGP_LOCAL=$(ip route get 8.8.8.8 | grep -Eo 'src [0-9.]+' | awk '{print $2}')
+export BGP_PEER=$(ip route get 8.8.8.8 | grep -Eo 'via [0-9.]+' | awk '{print $2}')
 
 sed -i "s/host_gw/$BGP_PEER/" /etc/exabgp/exabgp.conf
 sed -i "s/host_if/$BGP_LOCAL/" /etc/exabgp/exabgp.conf
